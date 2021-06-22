1fffd0b025a8e2f8f7722dbe8eba602ad3bb1125
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-22 11:46:20 -0300

    type object Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ aula7.js
@@ -0,0 +1,16 @@
+//type object
+
+const prod1 = {}
+
+console.log(typeof prod1)
+prod1.nome = 'celular'
+prod1.valor = 999.99
+prod1.categoria = 'eletronico'
+console.log(prod1)
+
+const prod2 = {
+nome: 'ps5',
+valor: 4.999,
+categoria: "eletronicos"
+}
+console.log(prod2)
