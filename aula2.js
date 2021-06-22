6d8113d5d82ef56c9c12abf967c276d71f8cb7f3
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-22 10:21:38 -0300

    type string
    Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ aula2.js
@@ -0,0 +1,10 @@
+// String
+
+const nome = 'allan'
+console.log(nome.charAt(2))
+console.log(nome.charAt(6))
+console.log(nome.charCodeAt(3))
+console.log(nome.charCodeAt(2))
+console.log(nome.substring(2))
+console.log(nome.substring(2, 4))
+console.log(nome.replace('ll', 11))
