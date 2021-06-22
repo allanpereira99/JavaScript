4e0f677409d7749d587b8efa9de640d752107551
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-22 13:13:26 -0300

    type null and undefined Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ aula8.js
@@ -0,0 +1,15 @@
+//type null and undefined
+
+
+let valor // não inicializada
+console.log(valor)
+try{
+console.log(valor2) //não definida
+}
+catch{
+    console.log('not defined')
+}
+finally{
+let valor3 = null // ausencia de valor
+console.log(valor3)
+}
