255c24aa0417b8ae03213ab65becbde694336a71
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-22 10:30:22 -0300

    Template String Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ aula3.js
@@ -0,0 +1,9 @@
+// Template String
+
+let nome = 'allan'
+let idade = 22
+let país = 'Brasil'
+
+console.log(`OI eu sou ${nome} e tenho ${idade} anos de idade e sou do ${país}`)
+
+console.log(`A soma de 1 + 1 = ${1+1}`)
