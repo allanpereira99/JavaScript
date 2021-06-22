ae2716915e654fb16c1a9e84fae5ee02fc2f7cea
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-22 15:02:20 -0300

    function Arraow
    Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ aula10.js
@@ -0,0 +1,13 @@
+// Funções arrow
+
+const multiplicar = (a,b) => {
+    return a * b
+}
+
+console.log(multiplicar(15,20))
+
+//retorno implicito
+
+const sub = (a,b) => a-b
+
+console.log(sub(15,5))
