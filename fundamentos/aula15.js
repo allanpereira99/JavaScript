9679e1b208276e0d69a630629e8dcf59aa151231
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-24 10:23:58 -0300

    relational operator and organize classes in subfolders Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ fundamentos/aula15.js
@@ -0,0 +1,9 @@
+// Operadores relacionais
+
+console.log('01:', '1' == 1) // compara o valor
+console.log('02:','1' === 1 ) // compara valor e tipo
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+ console.log('03:', d1  == d2) // vai comparar o endereco de memoria 
+ console.log('04:', d1.getTime == d2.getTime)
