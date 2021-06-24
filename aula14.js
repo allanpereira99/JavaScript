0531f474f65c66ff1672f6803de6a19af63caaa3
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-24 09:56:00 -0300

    operadores aritimeticos Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ aula14.js
@@ -0,0 +1,14 @@
+// operadores Aritimeticos
+
+const [a,b,c,d] = [1,2,3,4]
+
+const soma = a + b + c + d
+console.log(soma)
+const subtracao = a - b - c- d
+console.log(subtracao)
+const mutiplicacao = a * b* c * d
+console.log(mutiplicacao)
+const divisao = a / d
+console.log(divisao)
+const modulo = a % c //resto da divisao
+console.log(modulo)
