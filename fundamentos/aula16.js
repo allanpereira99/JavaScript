5693b9dd29f933946ab915ffbf640d4b9406e571
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-06-25 14:34:30 -0300

    operadores logicos Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com>

---
+++ fundamentos/aula16.js
@@ -0,0 +1,33 @@
+// Operadores logicos
+const n = 1
+const a = 1
+
+//operador 'e'
+if (a ==1 && n ==1){
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+//operador 'ou'
+if (a ==1 || n ==1){
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+//operador 'ou exclusivo' requer que exclusivamente um seja verdadeiro ou falso
+
+if (a ==1 ^ n ==1){
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+//operador unario 
+
+if (a ==1 != n ==1){
+    console.log('true')
+}else{
+    console.log('false')
+}
